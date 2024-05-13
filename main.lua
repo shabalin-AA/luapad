6e940c15f51828900e3f81edca8867e20a72c295
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2024-05-13 20:13:53 +0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+function last(t)
+  return t[#t]
+end
+
+function map(t, f)
+  local nt = {}
+  for k,v in pairs(t) do
+    nt[k] = f(v)
+  end
+  return nt
+end
+
 function clamp(v, minv, maxv)
   if     v < minv then return minv
   elseif v > maxv then return maxv
@@ -17,29 +29,27 @@ function write_file(fname, str)
   file:close()
 end
 
+local back_color = {0.1, 0.1, 0.12}
+
 local text = {}
 text.str = ''
 text.y = 0
 
-function get_text_line(n)
-  local line_beg = 0
-  for i=1,n-1 do
-    line_beg = string.find(text.str, '\n', line_beg+1)
-  end
-  local line_end = string.find(text.str, '\n', line_beg+1)
-  return text.str:sub(line_beg+1, line_end-1)
-end
-
-function text_lines()
-  local res = 0
-  for line in text.str:gmatch('\n') do
-    res = res + 1
-  end
-  return res
-end
-
-local font_size = 20
+local font_size = 22
 local font = nil
+
+local numbers = {}
+numbers.start = 1
+numbers.width = 0
+
+local cursor = {}
+cursor.position = {1,0,0} -- line, char in line, char in whole text
+cursor.t = os.clock() -- for blinking
+
+local selection = {}
+selection.active = false
+selection.intervals = {}
+
 
 function update_font()
   font = love.graphics.newFont('Menlo.ttc', font_size)
@@ -50,84 +60,106 @@ function lines_on_screen()
   return math.floor(love.graphics.getHeight() / font:getHeight())
 end
 
-local start_number = 1
-local numbers_width = 0
-
-function numbers_text()
-  local end_number = start_number + lines_on_screen()
-  local last_number = text_lines()
-  local delimiter = '| '
-  numbers_width = font:getWidth(' '..end_number..delimiter)
-  local res = ''
-  for i=start_number,math.min(end_number, last_number) do
-    local s = string.format(' %d%s\n', i, delimiter)
-    res = res..s
+function text:get_line(n)
+  local line_beg = 0
+  for i=1,n-1 do
+    line_beg = self.str:find('\n', line_beg+1)
   end
-  for i=last_number,end_number do
-    local s = string.format('~ %s\n', delimiter)
-    res = res..s
+  local line_end = self.str:find('\n', line_beg+1)
+  return self.str:sub(line_beg+1, line_end-1)
+end
+
+function text:count_lines()
+  local res = 0
+  for _line in self.str:gmatch('\n') do
+    res = res + 1
   end
   return res
 end
 
-local cursor = {}
-cursor.position = {1,0,0} -- line, char in line, char in whole text
-
-function move_text(y)
+function text:move(y)
   local font_height = font:getHeight()
-  text.y = text.y + y*font_height
-  text.y = clamp(text.y, -(text_lines()-1)*font_height, 0)
-  start_number = 1 - text.y/font_height
-  cursor.position[1] = clamp(cursor.position[1], start_number, start_number + lines_on_screen()-1)
+  self.y = self.y + y*font_height
+  self.y = clamp(text.y, -(self:count_lines()-1)*font_height, 0)
+  numbers.start = 1 - self.y/font_height
+  cursor.position[1] = clamp(cursor.position[1], numbers.start, numbers.start + lines_on_screen()-1)
+end
+
+function text:insert(t)
+  local before = self.str:sub(1,cursor.position[3])
+  local after  = self.str:sub(cursor.position[3]+1,-1)
+  self.str = before..t..after
+  cursor.position[2] = cursor.position[2] + #t
+  cursor:update()
+end
+
+function text:remove(pos1, pos2)
+  pos1 = clamp(pos1-1, 0, pos1-1)
+  pos2 = clamp(pos2+1, pos2+1, #self.str)
+  local before = self.str:sub(1, pos1)
+  local after = self.str:sub(pos2, -1)
+  self.str = before..after
+end
+
+function numbers:str()
+  local end_number = self.start + lines_on_screen()
+  local delimiter = '|'
+  self.width = font:getWidth(' '..end_number..delimiter)
+  local last_number = text:count_lines()
+  local res = ''
+  for i=self.start,math.min(end_number, last_number) do
+    res = res..string.format(' %d%s\n', i, delimiter)
+  end
+  for i=last_number,end_number do
+    res = res..string.format('~ %s\n', delimiter)
+  end
+  return res
 end
 
 function cursor:blink()
   local elapsed = os.clock() - self.t
   return 0 < elapsed%0.1 and elapsed%0.1 < 0.05 
 end
-cursor.t = os.clock()
 
 function cursor:draw()
   if self:blink() then
     love.graphics.setLineWidth(1)
-    love.graphics.setColor(1,1,1)
-    local x = numbers_width + font:getWidth(string.rep(' ', self.position[2]))
-    local y1 = (self.position[1]+0-start_number)*font:getHeight()
-    local y2 = (self.position[1]+1-start_number)*font:getHeight()
+    local x = numbers.width + font:getWidth(string.rep(' ', self.position[2]))
+    local y1 = text.y + (self.position[1]-1)*font:getHeight()
+    local y2 = text.y + (self.position[1]-0)*font:getHeight()
     love.graphics.line(x,y1,x,y2)
     self.t = 0
   end
-  love.graphics.setColor(1,0,0)
-  love.graphics.print(table.concat(self.position, '; '), 1000, 0)
+  local position_text = table.concat(self.position, ':')
+  love.graphics.print(
+    position_text, 
+    love.graphics.getWidth()-font:getWidth(position_text), 
+    love.graphics.getHeight()-font:getHeight()
+  )
 end
 
 function cursor:update()
-  cursor.position[1] = clamp(cursor.position[1], 1, text_lines())
-  cursor.position[2] = clamp(cursor.position[2], 0, #get_text_line(cursor.position[1]))
-  cursor.position[3] = 0
-  for i=1,cursor.position[1]-1 do
-    cursor.position[3] = cursor.position[3] + #get_text_line(i) + 1
+  self.position[1] = clamp(self.position[1], 1, text:count_lines())
+  self.position[2] = clamp(self.position[2], 0, #text:get_line(self.position[1]))
+  self.position[3] = 0
+  for i=1,self.position[1]-1 do
+    self.position[3] = self.position[3] + #text:get_line(i) + 1
   end
-  cursor.position[3] = cursor.position[3] + cursor.position[2]
-  local cursor_y = text.y + cursor.position[1]*font:getHeight()
-  if cursor_y < font:getHeight() then 
-    move_text(1)
-  elseif cursor_y > love.graphics.getHeight() then
-    move_text(-1)
-  end
-  cursor.t = os.clock()
+  self.position[3] = self.position[3] + self.position[2]
+  local y_lines = self.position[1] - numbers.start
+  text:move(clamp(y_lines, 1, lines_on_screen()-1) - y_lines)
+  self.t = os.clock()
 end
 
-function text:insert(t)
-  local before = text.str:sub(1,cursor.position[3])
-  local after  = text.str:sub(cursor.position[3]+1,#text.str)
-  text.str = before..t..after
-  cursor.position[2] = cursor.position[2] + #t
-  cursor:update()
+function selection:draw()
+  local selection_color = map(back_color, function(x) return (x+0.5) end)
+  love.graphics.setColor(selection_color)
+  -- love.graphics.rectangle('fill', 100, 100, 150, 150)
 end
+
 
 function love.load()
-  love.graphics.setBackgroundColor(0.1, 0.1, 0.12)
+  love.graphics.setBackgroundColor(back_color)
   update_font()
   text.str = read_file(arg[2])
   text.str = text.str..'\n'
@@ -136,17 +168,25 @@ function love.load()
 end
 
 function love.draw()
+  love.graphics.setColor(0.25,0.25,0.25)
+  love.graphics.printf(numbers:str(), 0,0, numbers.width, 'right')
+  selection:draw()
   love.graphics.setColor(1,1,1)
-  love.graphics.printf(numbers_text(), 0,0, numbers_width, 'right')
-  love.graphics.print(text.str, numbers_width, text.y)
+  love.graphics.print(text.str, numbers.width, text.y)
   cursor:draw()
 end
 
 function love.wheelmoved(x,y)
-  move_text(y)
+  text:move(y)
 end
 
 function love.keypressed(key)
+  selection.active = false
+  selection.intervals = {}
+  if key == 'lshift' then 
+    selection.active = true 
+    table.insert(selection.intervals, {})
+  end
   if love.keyboard.isDown('lgui') then
     if key == '=' then 
       font_size = font_size+1
@@ -159,15 +199,25 @@ function love.keypressed(key)
     elseif key == 'left' then 
       cursor.position[2] = 0
     elseif key == 'right' then
-      cursor.position[2] = #get_text_line(cursor.position[1])
+      cursor.position[2] = #text:get_line(cursor.position[1])
     elseif key == 'up' then
       cursor.position[1] = 1
       cursor.position[2] = 0
     elseif key == 'down' then
-      cursor.position[1] = text_lines()
-      cursor.position[2] = #get_text_line(cursor.position[1])
+      cursor.position[1] = text:count_lines()
+      cursor.position[2] = #text:get_line(cursor.position[1])
+    elseif key == 'v' then
+      text:insert(love.system.getClipboardText())
+    elseif key == 'c' then
+      -- love.system.setClipboardText(selection.str)
+    elseif key == 'backspace' then
+      text:remove(cursor.position[3]-cursor.position[2]+1, cursor.position[3])
+      cursor.position[2] = 0
     end
   else
+    if selection.active then 
+      table.insert(last(selection.intervals), cursor.position[3]) 
+    end
     if key == 'left' then
       cursor.position[2] = cursor.position[2] - 1
     elseif key == 'right' then
@@ -177,16 +227,12 @@ function love.keypressed(key)
     elseif key == 'down' then
       cursor.position[1] = cursor.position[1] + 1
     elseif key == 'backspace' then
-      local before = text.str:sub(1,cursor.position[3]-1)
-      local after  = text.str:sub(cursor.position[3]+1,#text.str)
-      text.str = before..after
-      if cursor.position[2] == 0 then
+      text:remove(cursor.position[3], cursor.position[3])
+      if cursor.position[2] == 0 then 
         cursor.position[1] = cursor.position[1] - 1
-        cursor:update()
-        cursor.position[2] = #get_text_line(cursor.position[1])
-        cursor:update()
+        cursor.position[2] = #text:get_line(cursor.position[1])
       else
-        cursor.position[2] = cursor.position[2] - 1
+        cursor.position[2] = cursor.position[2]-1
       end
     elseif key == 'return' then
       text:insert('\n')
@@ -201,3 +247,30 @@ end
 function love.textinput(t)
   text:insert(t)
 end
+
+function love.keyreleased(key)
+  if key == 'lshift' then 
+    selection.active = false 
+  end
+end
+
+function love.mousepressed(mx, my, button)
+  if mx > numbers.width then
+    cursor.position = {
+      1 + math.floor(my / font:getHeight()),
+      math.floor((mx - numbers.width) / font:getWidth(' '))
+    }
+    cursor:update()
+  end
+end
+
+
+--[[
+
+  TODO:
+1. selection
+2. code highlighting
+3. go to line (status bar)
+4. substitution
+
+]]
